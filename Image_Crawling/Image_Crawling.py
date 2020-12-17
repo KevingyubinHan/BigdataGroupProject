a3bec1722cd01be41b9504c4f2197fe9438a035b
Gyu bin Han <zbva027@live.rhul.ac.uk>
2020-12-17 10:20:27 +0900

    Image_crawling complete

--- Image_Crawling/Image_Crawling.py
+++ Image_Crawling/Image_Crawling.py
@@ -36,7 +36,7 @@ def scroll_down():
     scroll_count = 0
     print("scroll down() : 스크롤 다운 시작")
     
-    last_height = wd.execute_script("return document.bodt.scrollHeight")
+    last_height = wd.execute_script("return document.body.scrollHeight")
     after_click = False 
     
     while True : 
@@ -48,23 +48,23 @@ def scroll_down():
         new_height = wd.execute_script("return document.body.scrollHeight")
         
         
-        if last_heigt == new_height: # scroll down이 실행되지 않았다
+        if last_height == new_height: # scroll down이 실행되지 않았다
             if after_click is True:
                 break
             
             else:
                 try :
-                more_button = wd.find_element_by_xpath('//*[@id="islm"]/div/div/div/div/div[5]/input')
+                    more_button = wd.find_element_by_xpath('//*[@id="islmp"]/div/div/div/div/div[5]/input')
                 
                 
-                if more_button.is_displayed():
-                    more_button.click()
-                    after_click = True
-                    
+                    if more_button.is_displayed():
+                        more_button.click()
+                        after_click = True
+                        
                 except NoSuchElementException as e :
-                    print("exception is ", e)
-                    break
-        
+                        print("exception is ", e)
+                        break
+            
         last_height = new_height
         
 
@@ -73,40 +73,46 @@ def click_and_save(dir_name,index,img, img_list_length):
     global scrapped_count
     
     try:
-    img.click()
-    wd.implicitly_wait(3) # 클릭후 대기시간 3초
-    src = wd.find_element_by_xpath('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute('src')
-    
-    if src.split('.')[-1] == 'png':
-        urlretrieve(src,dir_name + '/' + str(scrapped_count+1)+ ".png")
-        print(f"{index+1}/{img_list_length} PNG 이미지 저장")
-    else:
-        urlretrieve(src,dir_name + '/' + str(scrapped_count+1) + ".jpg")
-        print(f"{index+1}/{img_list_length} jpg 이미지 저장")
-    scrapped_count += 1
-    
+        img.click()
+        wd.implicitly_wait(3) # 클릭후 대기시간 3초
+        src = wd.find_element_by_xpath('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute('src')
+        
+        if src.split('.')[-1] == 'png':
+            urlretrieve(src,dir_name + '/' + str(scrapped_count+1)+ ".png")
+            print(f"{index+1}/{img_list_length} PNG 이미지 저장")
+        else:
+            urlretrieve(src,dir_name + '/' + str(scrapped_count+1) + ".jpg")
+            print(f"{index+1}/{img_list_length} jpg 이미지 저장")
+        scrapped_count += 1
+        
     except HTTPError as e:
         print(e)
         pass
+    except ElementClickInterceptedException as e:
+        print(e)
+        wd.execute_script("window.scrollTo(0,window.scrollY + 100")
+        wd.sleep(1)
+        click_and_save(dir_name, index, img,len(img_list))
+        
     
 # filter_and_remove : 일정 해상도 이하이거나 손상된 이미지 제거
 def filter_and_remove(dir_name,query,filter_size):
     filtered_count = 0
     
-        for index, file_name in enumerate(os.listdir(dir_name)):
-            file_path = os.path.join(dir_name,file_name)
-            img = Image.open(file_path)
-            try:
-                if img.width < filter_size and img.height < filter_size :
-                    img.close()
-                    os.remove(file_path)
-                    print(f"{index} 이미지 제거")
-                    filtered_count += 1
-            except OSError as e:
-                print(e)
+    for index, file_name in enumerate(os.listdir(dir_name)):
+        file_path = os.path.join(dir_name,file_name)
+        img = Image.open(file_path)
+        try:
+            if img.width < filter_size and img.height < filter_size :
+                img.close()
                 os.remove(file_path)
+                print(f"{index} 이미지 제거")
                 filtered_count += 1
-        print(f"이미지 제거 개수 : {filtered_count}/{scrapped_count}")
+        except OSError as e:
+            print(e)
+            os.remove(file_path)
+            filtered_count += 1
+    print(f"이미지 제거 개수 : {filtered_count}/{scrapped_count}")
         
     
         
@@ -117,16 +123,16 @@ def scraping(dir_name,query):
     global scrapped_count
     
     url = f"https://www.google.com/search?q={query}&tbm=isch&ved=2ahUKEwi4pYDrptHtAhVKAKYKHbfJA6gQ2-cCegQIABAA&oq=sea&gs_lcp=CgNpbWcQAzIFCAAQsQMyAggAMgUIABCxAzICCAAyAggAMgUIABCxAzICCAAyAggAMggIABCxAxCDATICCABQ1w5Y9hBgwhRoAHAAeACAAXyIAeMCkgEDMC4zmAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=nFzZX7jnEsqAmAW3k4_ACg&bih=721&biw=1280"
-    print(url)
     wd.get(url)
     wd.maximize_window()
     
     scroll_down() # 스크롤 다운
     
     div = wd.find_element_by_xpath('//*[@id="islrg"]/div[1]')
-    img_list = div.find_element_by_xpath('//*[@id="islrg"]/div[1]/div[1]/a[1]/div[1]/img')
+    img_list = div.find_elements_by_css_selector('div.bRMDJf.islir > img')
     
     for index, img in enumerate(img_list):
+        
         try:
             click_and_save(dir_name, index, img,len(img_list)) # 이미지 다운로드
             
@@ -177,20 +183,16 @@ def scraping(dir_name,query):
 
 
 # driver 홈페이지 접근
-wd = webdriver.Chrome("/Users/Han/Download2/chromedriver")
+wd = webdriver.Chrome("/Users/Han/Download2/chromedriver",options=chrome_options)
 
 scrapped_count = 0 # 갯수 초기화
-path = "/Users/Han/Download2/BigdataGroupProject/BigdataGroupProject/images"
+path = "/Users/Han/Download2/BigdataGroupProject/BigdataGroupProject/images/"
 query = input("검색어 입력 : ")
 
 dir_name = path + query
 os.makedirs(dir_name)
 print(f"{dir_name}디렉토리 생성")
 
-    url = f"https://www.google.com/search?q={query}&tbm=isch&ved=2ahUKEwi4pYDrptHtAhVKAKYKHbfJA6gQ2-cCegQIABAA&oq=sea&gs_lcp=CgNpbWcQAzIFCAAQsQMyAggAMgUIABCxAzICCAAyAggAMgUIABCxAzICCAAyAggAMggIABCxAxCDATICCABQ1w5Y9hBgwhRoAHAAeACAAXyIAeMCkgEDMC4zmAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=nFzZX7jnEsqAmAW3k4_ACg&bih=721&biw=1280"
-    wd.get(url)
-    wd.maximize_window()
-scroll_down()
 scraping(dir_name, query)
 filter_and_remove(dir_name,query,400)
 

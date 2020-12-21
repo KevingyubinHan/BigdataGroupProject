48991cca28b0974a1ced38f05c204c12dbf54c61
LeeSuWan626 <75827037+LeeSuWan626@users.noreply.github.com>
2020-12-21 22:58:25 +0900

    Connect Model, Opencv and Pyqt5
    모델, Opencv pyqt5 연동 완료

---
+++ Connect_Opencv_Pyqt5.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 21 00:45:34 2020
+
+@author: Swan
+"""
+
+
+import cv2
+import threading
+import sys
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+from PyQt5 import QtCore
+import numpy as np   
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+
+
+!if not exist "./files" mkdir files
+# Download Face detection XML 
+!curl -L -o ./files/haarcascade_frontalface_default.xml https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml
+# Download emotion trained data
+!curl -L -o ./files/emotion_model.hdf5 https://mechasolution.vn/source/blog/AI-tutorial/Emotion_Recognition/emotion_model.hdf5
+
+
+
+# Face detection XML load and trained model loading
+face_detection = cv2.CascadeClassifier('files/haarcascade_frontalface_default.xml')
+emotion_classifier = load_model('C:/Users/Swan/Documents/GitHub/BigdataGroupProject/ai_interview_model.hdf5', compile=False)
+EMOTIONS = ["Angry", "Disgusting", "Fearful", "Happy", "Sad", "Surprising", "Neutral"]
+
+
+running = False
+def run():
+    global running
+    camera = cv2.VideoCapture(0)
+    
+    while True:
+        # Capture image from camera
+        ret, frame = camera.read()
+        
+        # Convert color to gray scale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        # Face detection in frame
+        faces = face_detection.detectMultiScale(gray,
+                                                scaleFactor=1.1,
+                                                minNeighbors=5,
+                                                minSize=(30,30))
+        
+        # Create empty image
+        canvas = np.zeros((250, 300, 3), dtype="uint8")
+        
+        # Perform emotion recognition only when face is detected
+        if len(faces) > 0:
+            # For the largest image
+            face = sorted(faces, reverse=True, key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
+            (fX, fY, fW, fH) = face
+            # Resize the image to 48x48 for neural network
+            roi = gray[fY:fY + fH, fX:fX + fW]
+            roi = cv2.resize(roi, (48, 48))
+            roi = roi.astype("float") / 255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi, axis=0)
+            
+            # Emotion predict
+            preds = emotion_classifier.predict(roi)[0]
+            emotion_probability = np.max(preds)
+            label = EMOTIONS[preds.argmax()]
+            
+            #print(preds)
+            #[0.01607699 0.00203081 0.01832145 0.79570323 0.04124354 0.00822391 0.1184001 ]
+            #print(emotion_probability)
+            #print(label)
+            
+            
+            # Assign labeling
+            cv2.putText(frame, label, (fX, fY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+            cv2.rectangle(frame, (fX, fY), (fX + fW, fY + fH), (0, 0, 255), 2)
+            
+            # Label printing
+            for (i, (emotion, prob)) in enumerate(zip(EMOTIONS, preds)):
+                text = "{}: {:.2f}%".format(emotion, prob * 100)    
+                w = int(prob * 300)
+                cv2.rectangle(canvas, (7, (i * 35) + 5), (w, (i * 35) + 35), (0, 0, 255), -1)
+                cv2.putText(canvas, text, (10, (i * 35) + 23), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 255), 2)
+    
+        # Open two windows
+        ## Display image ("Emotion Recognition")
+        ## Display probabilities of emotion
+        cv2.imshow('Emotion Recognition', frame)
+        cv2.imshow("Probabilities", canvas)
+        
+        # q to quit
+        key = cv2.waitKey(1) & 0xFF
+        if (preds[3] < 0.5): #happy가 50퍼 이하면 종료 , 대신 첨부터 컴퓨터 화면을 보고 있어야 함.
+           break
+       # happy가 너무 민감함 -> modeling을 통해 잡아야함
+    camera.release()
+    cv2.destroyAllWindows()
+    print("Thread end.")
+
+def stop():
+    global running
+    running = False
+    print("stoped..")
+    
+# threading으로 실시간 반영
+def start():
+    global running
+    running = True
+    th = threading.Thread(target=run)
+    th.start()
+    print("started..")
+
+def onExit():
+    print("exit")
+    stop()
+
+app = QtWidgets.QApplication([])
+win = QtWidgets.QWidget()
+vbox = QtWidgets.QVBoxLayout()
+label = QtWidgets.QLabel()
+btn_start = QtWidgets.QPushButton("AI interview Test Start")
+btn_stop = QtWidgets.QPushButton("AI interview Test End")
+vbox.addWidget(label)
+vbox.addWidget(btn_start)
+vbox.addWidget(btn_stop)
+win.setLayout(vbox)
+win.show()
+
+btn_start.clicked.connect(start)
+btn_stop.clicked.connect(stop)
+app.aboutToQuit.connect(onExit)
+
+sys.exit(app.exec_())

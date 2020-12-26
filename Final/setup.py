014ec04e1796c5a84896a787f710ecee8a9be7e4
LeeSuWan626 <75827037+LeeSuWan626@users.noreply.github.com>
2020-12-27 00:46:47 +0900

    Final
    create a final folder

---
+++ Final/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+exe 실행파일 생성
+
+@author: Swan
+"""
+
+from distutils.core import setup
+import py2exe, sys, os
+
+sys.argv.append('py2exe')
+
+setup(
+    options = {'py2exe': {'bundle_files': 1}},
+    windows = [{'script': "GUI_Qthread_5.py"}],
+    zipfile = None,
+)
+
+python setup.py 
